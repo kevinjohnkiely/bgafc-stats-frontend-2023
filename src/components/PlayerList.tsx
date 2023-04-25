7a6daeb982ca58fb1771bae2f4d650cac0ef61c9
Kevin John Kiely <kevinjohnkiely1@hotmail.com>
2023-04-25 16:53:19 +0100

    Created player list table

--- src/components/PlayerList.tsx
+++ src/components/PlayerList.tsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Player } from './../models/player';
+import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table';
+import styles from './../styles/PlayerList.module.css';
 
 const PlayerList = () => {
   const [players, setPlayers] = useState<Player[]>([]);
@@ -15,10 +18,42 @@ const PlayerList = () => {
     fetchPlayers().catch(console.error);
   }, []);
   return (
-    <div style={{backgroundColor: 'white'}}>
-      <h1>Players:</h1>
-      {players.map(player => <p>{player.firstName} {player.lastName}</p>)}
-    </div>
+    <Container fluid className={styles.playerListLayout}>
+      <h3>All-Time Player Statistics 1984 – present</h3>
+      <Table
+        striped
+        bordered
+        hover
+        style={{ width: '88%', margin: 'auto', textAlign: 'center' }}
+      >
+        <thead
+          style={{ background: '#339900', color: 'white', fontSize: '13px' }}
+        >
+          <tr>
+            <th style={{ width: '50%' }}>Player (Sort A-Z)</th>
+            <th>A Team Apps</th>
+            <th>A Team Goals</th>
+            <th>B Team Apps</th>
+            <th>B Team Goals</th>
+            <th>Total Apps</th>
+            <th>Total Goals</th>
+          </tr>
+        </thead>
+        <tbody>
+          {players.map(player => (
+            <tr>
+              <td><strong>{player.lastName.toUpperCase()}</strong>, {player.firstName}</td>
+              <td>{player.aTeamApps}</td>
+              <td>{player.aTeamGoals}</td>
+              <td>{player.bTeamApps}</td>
+              <td>{player.bTeamGoals}</td>
+              <td>{player.aTeamApps + player.bTeamApps}</td>
+              <td>{player.aTeamGoals + player.bTeamGoals}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Container>
   );
 };
 
