283a923ebb8743b09756d035036b83a09f1779cc
Kevin John Kiely <kevinjohnkiely1@hotmail.com>
2023-04-26 22:09:17 +0100

    Tidy up css on navbar

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -3,13 +3,15 @@ import styles from './../styles/MainNav.module.css'
 
 const MainNav = () => {
   return (
-    <Navbar className={styles.navBGColor} expand='lg'>
+    <Navbar className={styles.navBGColor} expand='sm'>
       <Container>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav'>
           <Nav className='me-auto'>
             <Nav.Link href='#home'>Main Site</Nav.Link>
-            <Nav.Link href='#link'>Link</Nav.Link>
+            <Nav.Link href='#link'>All Players</Nav.Link>
+            <Nav.Link href='#link'>Add A Player</Nav.Link>
+            <Nav.Link href='#link'>Login/Logout</Nav.Link>
             <NavDropdown title='Dropdown' id='basic-nav-dropdown'>
               <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
               <NavDropdown.Item href='#action/3.2'>
