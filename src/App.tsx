fcc870f2c5571e1677398eda4326e91917dd82e3
Kevin John Kiely <kevinjohnkiely1@hotmail.com>
2023-05-23 09:56:28 +0100

    Added user model

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,39 @@
+import { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
-import PlayerList from './pages/PlayerList';
+import { Route, Routes } from 'react-router-dom';
 import header from './assets/images/header.jpg';
-import styles from './styles/App.module.css';
 import MainNav from './components/MainNav';
-import { Routes, Route } from 'react-router-dom';
-import PlayerSingle from './pages/PlayerSingle';
 import NotFoundPage from './pages/NotFoundPage';
+import PlayerList from './pages/PlayerList';
+import PlayerSingle from './pages/PlayerSingle';
+import styles from './styles/App.module.css';
+
+import { User } from './models/user';
 
 const App = () => {
+  const [user, setUser] = useState<User>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>('');
+  useEffect(() => {
+    const getLoggedInUser = async () => {
+      setLoading(true);
+      const data = await fetch('/api/v1/users/getuser');
+      if (data.status === 500) {
+        setError('Server Error: Please try again soon.');
+        setLoading(false);
+      }
+      const json = await data.json();
+      if (json.message) {
+        setError(json.message);
+      } else {
+        setUser(json.data.user);
+      }
+      setLoading(false);
+    };
+    getLoggedInUser();
+  }, []);
+
   return (
     <Container className={styles.pageMaxWidth}>
       <header className={styles.pageMarginTop}>
